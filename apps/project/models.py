33203a4e887b9240bd8b115f067ed71e7214f2c9
Konstantinos Vitiniotis <kostasvit@gmail.com>
2024-01-24 13:01:03 +0200

    css changes

--- apps/project/models.py
+++ apps/project/models.py
@@ -1,4 +1,4 @@
-import uuid
+import uuid,os
 from datetime import datetime
 from django.db import models
 from accounts.models import CustomUser
@@ -20,12 +20,20 @@ class Project(TimeStampMixin):
     def __str__(self):
         return self.name
 
+def custom_upload_path(instance, filename):
+    # Get the related model's name and use it in the file path
+    project = instance.project.name if instance.project else 'no_related_name'
+    sanitized_model_name = project.replace(' ', '_').replace('-', '_').replace('|', '_')  # Add more replacements as needed
+    
+    # Use os.path.join for constructing the file path
+    return os.path.join('custom_directory', sanitized_model_name, filename)
+    #return f'custom_directory/{project}/{filename}'
 
 class ProjectFile(TimeStampMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     project = models.ForeignKey(Project, related_name='files', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    attachment = models.FileField(upload_to='projectfiles')
+    attachment = models.FileField(upload_to=custom_upload_path)
 
     def __str__(self):
         return self.name
